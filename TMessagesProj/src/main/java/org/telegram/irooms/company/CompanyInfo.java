81364d1ea489e9e6039540398127a4c6842f1a4c
shuhrat <shuh100@gmail.com>
2021-01-21 12:17:34 +0500

    When removing member if last name is null, null value was appearing with firstname. null removed from UI

--- TMessagesProj/src/main/java/org/telegram/irooms/company/CompanyInfo.java
+++ TMessagesProj/src/main/java/org/telegram/irooms/company/CompanyInfo.java
@@ -3,22 +3,15 @@ package org.telegram.irooms.company;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
-import android.text.InputType;
-import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.exoplayer2.util.Log;
-
 import org.telegram.irooms.IRoomsManager;
 import org.telegram.irooms.database.Company;
 import org.telegram.messenger.AndroidUtilities;
-import org.telegram.messenger.LocaleController;
-import org.telegram.messenger.MessagesController;
 import org.rooms.messenger.R;
 import org.telegram.tgnet.TLRPC;
 import org.telegram.ui.ActionBar.ActionBar;
@@ -26,17 +19,12 @@ import org.telegram.ui.ActionBar.AlertDialog;
 import org.telegram.ui.ActionBar.BaseFragment;
 import org.telegram.ui.ActionBar.Theme;
 import org.telegram.ui.ActionBar.ThemeDescription;
-import org.telegram.ui.Cells.LoadingCell;
-import org.telegram.ui.Cells.TextInfoPrivacyCell;
-import org.telegram.ui.Components.EditTextBoldCursor;
-import org.telegram.ui.Components.EmptyTextProgressView;
 import org.telegram.ui.Components.LayoutHelper;
 import org.telegram.ui.Components.RecyclerListView;
 import org.telegram.ui.Components.voip.VoIPHelper;
 import org.telegram.ui.LaunchActivity;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import androidx.recyclerview.widget.LinearLayoutManager;
 import io.socket.client.Socket;
@@ -108,8 +96,10 @@ public class CompanyInfo extends BaseFragment {
                 AlertDialog.Builder builder = new AlertDialog.Builder(getParentActivity());
                 builder.setTitle("Are you sure you want to delete this user?");
                 TextView info = new TextView(getParentActivity());
-                info.setPadding(34, 0, 0, 0);
-                info.setText(user.first_name + " " + user.last_name);
+                info.setPadding(54, 0, 0, 0);
+                String fullName = user.first_name != null ? user.first_name : "" + user.last_name != null ? user.last_name : "";
+
+                info.setText(fullName);
                 builder.setView(info);
                 builder.setPositiveButton("Yes", (dialogInterface, i) -> {
                     removeMemberFromCompany(position, user.id);
@@ -138,8 +128,8 @@ public class CompanyInfo extends BaseFragment {
     private void removeMemberFromCompany(int position, int id) {
         ArrayList<Integer> members = new ArrayList<>(1);
         members.add(id);
-        Socket socket =((LaunchActivity)getParentActivity()).getmSocket();
-        IRoomsManager.getInstance().deleteMembersFromCompany(getParentActivity(),socket, mCompany.getId(), members, new IRoomsManager.IRoomsCallback() {
+        Socket socket = ((LaunchActivity) getParentActivity()).getmSocket();
+        IRoomsManager.getInstance().deleteMembersFromCompany(getParentActivity(), socket, mCompany.getId(), members, new IRoomsManager.IRoomsCallback() {
             @Override
             public void onSuccess(String success) {
                 memberAdapter.removeItem(position);
@@ -150,58 +140,6 @@ public class CompanyInfo extends BaseFragment {
                 Toast.makeText(getParentActivity(), error, Toast.LENGTH_LONG).show();
             }
         });
-    }
-
-    private EditTextBoldCursor getEditText(Context context) {
-
-        EditTextBoldCursor editText = new EditTextBoldCursor(context);
-        editText.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 18);
-        editText.setHintTextColor(Theme.getColor(Theme.key_windowBackgroundWhiteHintText));
-        editText.setTextColor(Theme.getColor(Theme.key_windowBackgroundWhiteBlackText));
-        editText.setBackground(Theme.createEditTextDrawable(context, false));
-        editText.setMaxLines(1);
-        editText.setLines(1);
-        editText.setPadding(0, 0, 0, 0);
-        editText.setSingleLine(true);
-        editText.setGravity(LocaleController.isRTL ? Gravity.RIGHT : Gravity.LEFT);
-        editText.setInputType(InputType.TYPE_TEXT_FLAG_CAP_SENTENCES | InputType.TYPE_TEXT_FLAG_MULTI_LINE | InputType.TYPE_TEXT_FLAG_AUTO_CORRECT);
-        editText.setImeOptions(EditorInfo.IME_ACTION_DONE);
-        editText.setHint("Company name");
-        editText.setCursorColor(Theme.getColor(Theme.key_windowBackgroundWhiteBlackText));
-        editText.setCursorSize(AndroidUtilities.dp(20));
-        editText.setCursorWidth(1.5f);
-        return editText;
-    }
-
-    private TextView getCreateCompanyButton(Context context) {
-        TextView buttonTextView = new TextView(context);
-        buttonTextView.setText("Create a new company");
-        buttonTextView.setPadding(AndroidUtilities.dp(34), 0, AndroidUtilities.dp(34), 0);
-        buttonTextView.setGravity(Gravity.CENTER);
-        buttonTextView.setTextColor(Theme.getColor(Theme.key_featuredStickers_buttonText));
-        buttonTextView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 14);
-        buttonTextView.setTypeface(AndroidUtilities.getTypeface("fonts/rmedium.ttf"));
-        buttonTextView.setBackground(Theme.createSimpleSelectorRoundRectDrawable(AndroidUtilities.dp(4), Theme.getColor(Theme.key_featuredStickers_addButton), Theme.getColor(Theme.key_featuredStickers_addButtonPressed)));
-        return buttonTextView;
-    }
-
-    private void confirmAndDelete(final CallLogRow row) {
-        if (getParentActivity() == null) {
-            return;
-        }
-        new AlertDialog.Builder(getParentActivity())
-                .setTitle(LocaleController.getString("AppName", R.string.AppName).replace("Telegram","Rooms"))
-                .setMessage(LocaleController.getString("ConfirmDeleteCallLog", R.string.ConfirmDeleteCallLog))
-                .setPositiveButton(LocaleController.getString("Delete", R.string.Delete), (dialog, which) -> {
-                    ArrayList<Integer> ids = new ArrayList<>();
-                    for (TLRPC.Message msg : row.calls) {
-                        ids.add(msg.id);
-                    }
-                    MessagesController.getInstance(currentAccount).deleteMessages(ids, null, null, 0, 0, false, false);
-                })
-                .setNegativeButton(LocaleController.getString("Cancel", R.string.Cancel), null)
-                .show()
-                .setCanceledOnTouchOutside(true);
     }
 
     @Override
@@ -220,13 +158,6 @@ public class CompanyInfo extends BaseFragment {
                 VoIPHelper.permissionDenied(getParentActivity(), null, requestCode);
             }
         }
-    }
-
-    private static class CallLogRow {
-        public TLRPC.User user;
-        public List<TLRPC.Message> calls;
-        public int type;
-        public boolean video;
     }
 
     @Override
